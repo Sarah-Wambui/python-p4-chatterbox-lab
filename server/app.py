fd7bcf001fbefa33b3860b0647c709123f26253f
sarahwambui96 <sarahwambui843@gmail.com>
2023-06-30 17:09:06 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -14,13 +14,60 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == "GET":
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_list =[]
+        for message in messages:
+            message_dict = {
+            "id":message.id,
+            "body":message.body,
+            "username": message.username,
+            "created_at": message.created_at.isoformat()
+            }
+            messages_list.append(message_dict)
+        return make_response(jsonify(messages_list), 200)
+
+    elif request.method == "POST":
+         data = request.get_json()
+         new_message = Message(
+             body = data.get("body"),
+             username = data.get("username")
+         )
+         db.session.add(new_message)
+         db.session.commit()
+
+         message_dict = new_message.to_dict()
+         return make_response(jsonify(message_dict), 201)
+
+@app.route('/messages/<int:id>', methods = ["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id =id).first()
+
+    if message == None:
+        response_body = {
+            "message": "This record does not exist in our database."
+        }
+        return make_response(jsonify(response_body), 404)
+    
+    else:
+        if request.method == "PATCH":
+            data = request.get_json()
+            if "body" in data:
+                message.body = data.get("body")
+            db.session.commit()
+            message_dict = message.to_dict()
+            return make_response(jsonify(message_dict), 200)
+        
+        elif request.method == "DELETE":
+           message = Message.query.filter_by(id=id).first()
+           db.session.delete(message)
+           db.session.commit()
+
+           return "", 200 
+
 
 if __name__ == '__main__':
     app.run(port=5555)
